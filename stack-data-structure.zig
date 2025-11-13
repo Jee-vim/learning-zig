36b48ec61dfe984daddb3306b2d37f75f6649586
Jee <zeddnyx08@gmail.com>
2025-11-14 06:40:51 +0700

    wip: stack data structure (generic stack)

--- stack-data-structure.zig
+++ stack-data-structure.zig
@@ -43,4 +43,48 @@ const Stack = struct {
     }
 };
 
+// generic stack structure
+fn GenStack(comptime T: type) type {
+    return struct {
+        items: []T,
+        capacity: usize,
+        length: usize,
+        allocator: Allocator,
+        const Self = @This();
+
+        pub fn init(alloctor: Allocator, capacity: usize) !Stack(T) {
+            var buf = try alloctor.alloc(T, capacity);
+            return .{
+                .items = buf[0..],
+                .capacity = capacity,
+                .length = 0,
+                .allocator = alloctor,
+            };
+        }
+
+        pub fn deinit(self: *Stack) void {
+            self.allocator.free(self.items);
+        }
+
+        pub fn push(self: *Stack, val: T) !void {
+            if ((self.length + 1) > self.capacity) {
+                var new_buf = try self.allocator.alloc(T, self.capacity * 2);
+                @memcpy(new_buf[0..self.capacity], self.items);
+                self.allocator.free(self.items);
+                self.items = new_buf;
+                self.capacity = self.capacity * 2;
+            }
+
+            self.items[self.length] = val;
+            self.length += 1;
+        }
+
+        pub fn pop(self: *Stack) void {
+            if (self.length == 0) return;
+            self.items[self.length - 1] = undefined;
+            self.length -= 1;
+        }
+    };
+}
+
 pub fn main() !void {}
